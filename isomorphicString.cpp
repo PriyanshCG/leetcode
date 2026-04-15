b0a1791737c791f6ac6dfe13fe4a3681250fd24d
PriyanshCG <priyansh.patel.cg@gmail.com>
2026-04-15 10:23:08 +0530

    new question added

---
+++ isomorphicString.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool isIsomorphic(string s, string t) {
+        unordered_map<char, char>m1,m2;
+        for(int i=0; i<s.length(); ++i){
+            char ch1=s[i]; 
+            char ch2=t[i];
+            if(m1.count(ch1) && m1[ch1] != ch2)
+            return false;
+            if(m2.count(ch2) && m2[ch2] != ch1)
+            return false; 
+            m1[ch1]=ch2;
+            m2[ch2]=ch1;
+        }
+        return true;
+    }
+};
